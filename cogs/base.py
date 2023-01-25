11b052f2bdbc0b31e79cbeed5997cda78a5f2c4f
InvalidJokerDE <janniskoder@icloud.com>
2023-01-25 18:46:17 +0100

    Start coding the bot

---
+++ cogs/base.py
@@ -0,0 +1,21 @@
+import nextcord
+from nextcord.ext import commands
+
+class Base(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def ping(self, ctx: commands.Context):
+        await ctx.send("Pong!")
+
+    @nextcord.slash_command()
+    async def slash_ping(self, ctx: nextcord.Interaction):
+        await ctx.response.send_message("Pong!", ephemeral=True)
+
+    @commands.command()
+    async def say(self, ctx: commands.Context, *, message: str):
+        await ctx.send(f"{ctx.author.mention}: ```{message}```")
+
+def setup(bot):
+    bot.add_cog(Base(bot))
--- main.py
+++ main.py
@@ -1,16 +1,98 @@
-# This is a sample Python script.
+import nextcord
+from nextcord.ext import commands
+from config import TOKEN, PREFIX, ERROR_CHANNEL, OWNER
+import os
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+bot = commands.Bot(command_prefix=PREFIX, intents=nextcord.Intents.all(), case_insensitive=True, owner_ids=[OWNER])
 
+for i in os.listdir("./cogs"):
+    if i.endswith(".py"):
+        bot.load_extension(f"cogs.{i[:-3]}")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+@bot.event
+async def on_ready():
+    print(f"📡 | Bot ID: {bot.user.id} | Bot Name: {bot.user.name}\n❤️ | Trete meinem Discord bei: https://discord.gg/x8b26bTCd4")
 
+@bot.command()
+@commands.is_owner()
+async def load(ctx, extension):
+    try:
+        bot.load_extension(f"cogs.{extension}")
+    except commands.ExtensionAlreadyLoaded:
+        return await ctx.send("Cog is already loaded")
+    except commands.ExtensionNotFound:
+        return await ctx.send("Cog is not found")
+    await ctx.send("Cog loaded")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@bot.command()
+@commands.is_owner()
+async def reload(ctx, extension):
+    try:
+        bot.reload_extension(f"cogs.{extension}")
+    except commands.ExtensionNotFound:
+        return await ctx.send("Cog is not found")
+    await ctx.send("Cog reloaded")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@bot.command()
+@commands.is_owner()
+async def unload(ctx, extension):
+    try:
+        bot.unload_extension(f"cogs.{extension}")
+    except commands.ExtensionNotFound:
+        return await ctx.send("Cog is not found")
+    await ctx.send("Cog unloaded")
+
+@bot.command()
+@commands.is_owner()
+async def check(ctx, cog_name):
+    try:
+        bot.load_extension(f"cogs.{cog_name}")
+    except commands.ExtensionAlreadyLoaded:
+        await ctx.send("Cog is loaded")
+    except commands.ExtensionNotFound:
+        await ctx.send("Cog not found")
+    else:
+        await ctx.send("Cog is unloaded")
+        bot.unload_extension(f"cogs.{cog_name}")
+
+@bot.event
+async def on_application_command_error(ctx, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            em = nextcord.Embed(
+                title="⌚ × Cooldown",
+                description=f"» Du musst noch {round(error.retry_after)} Sekunden warten!",
+            )
+            await ctx.send(embed=em, delete_after=20)
+            return
+        else:
+            userem = nextcord.Embed(
+                title="❌ × Ein Fehler ist aufgetreten",
+                description=f"Beim Ausführen dieses Befehls ist ein Fehler aufgetreten. Bitte versuchen sie es später erneut!",
+            )
+            em = nextcord.Embed(title="❌ Fehler", description=f"```{error}```")
+            errorkanal = bot.get_channel(ERROR_CHANNEL)
+            await errorkanal.send(embed=em)
+            await ctx.send(embed=userem, delete_after=30)
+            return
+
+@bot.event
+async def on_command_error(ctx, error):
+        if isinstance(error, commands.NotOwner):
+            em = nextcord.Embed(
+                title="👑 × Du darfst diesen Befehl nicht ausführen",
+                description=f"Nur <@{OWNER}> darf diesen Command benutzen!",
+            )
+            await ctx.send(embed=em, delete_after=20)
+            return
+        else:
+            userem = nextcord.Embed(
+                title="❌ × Ein Fehler ist aufgetreten",
+                description=f"Beim Ausführen dieses Befehls ist ein Fehler aufgetreten. Bitte versuchen sie es später erneut!",
+            )
+            em = nextcord.Embed(title="❌ Fehler", description=f"```{error}```")
+            errorkanal = bot.get_channel(ERROR_CHANNEL)
+            await errorkanal.send(embed=em)
+            await ctx.send(embed=userem, delete_after=30)
+            return
+
+bot.run(TOKEN)
